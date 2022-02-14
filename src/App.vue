0d41452ea7da0d770a7b164cc87420af3bce8b3e
Meruem117 <1572677319@qq.com>
2022-02-14 21:02:23 +0800

    init amap

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AMap />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AMap from './components/AMap.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AMap
   }
 }
 </script>
@@ -20,7 +19,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/AMap.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="container"></div>
+</template>
+
+<script>
+import { shallowRef } from '@vue/reactivity'
+import AMapLoader from '@amap/amap-jsapi-loader'
+
+export default {
+  name: 'AMap',
+  props: {
+    msg: String
+  },
+  setup() {
+    const map = shallowRef(null)
+    return {
+      map,
+    }
+  },
+  methods: {
+    initMap() {
+      AMapLoader.load({
+        key: "1bef5e6bc5ac9412518943a96c8a1f28",
+        version: "2.0",
+        plugins: [''],
+      }).then((AMap) => {
+        this.map = new AMap.Map("container", {
+          viewMode: "3D",
+          zoom: 5,
+          center: [105.602725, 37.076636],
+        })
+      }).catch(e => {
+        console.log(e)
+      })
+    },
+  },
+  mounted() {
+    this.initMap()
+  }
+}
+</script>
+
+<style scoped>
+#container {
+  padding: 0px;
+  margin: 0px;
+  width: 100%;
+  height: 800px;
+}
+</style>
