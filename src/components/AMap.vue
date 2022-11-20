2930bd90d2f4400476da1175ca944587e2375a2a
Meruem117 <1572677319@qq.com>
2022-11-20 17:27:17 +0800

    set map full screen

--- src/components/AMap.vue
+++ src/components/AMap.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="page">
-    <div id="a-map" class="map"></div>
+    <div class="map-box">
+      <div id="a-map" class="map"></div>
+    </div>
   </div>
 </template>
 
@@ -52,13 +54,22 @@ export default {
 <style lang="less" scoped>
 .page {
   width: 100%;
-  height: 900px;
+  height: 100%;
   padding: 0px;
   margin: 0px;
 
-  .map {
-    width: 100%;
-    height: 100%;
+  .map-box {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+
+    .map {
+      width: 100%;
+      height: 100%;
+    }
   }
+
 }
 </style>
