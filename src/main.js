69a6af1568f34c2acaef8717226916a7279d310c
Meruem117 <1572677319@qq.com>
2022-11-24 00:28:32 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'vuetify/lib/styles'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import 'vuetify/styles'
+import * as components from 'vuetify/lib/components'
+import * as directives from 'vuetify/lib/directives'
 
 const vuetify = createVuetify({
   components,
