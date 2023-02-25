302b82b4c0c65ae31de580db097ae0a45703b78c
Meruem117 <1572677319@qq.com>
2023-02-26 00:28:24 +0800

    add polygon

--- src/views/BMap.vue
+++ src/views/BMap.vue
@@ -14,22 +14,49 @@ export default {
     name: 'BMap',
     setup() {
         const map = shallowRef(null)
-        const centerPoint = new BMapGL.Point(119.979759, 31.792228)
+        const city = {
+            name: '常州市',
+            center: new BMapGL.Point(119.64489, 31.629129)
+        }
         return {
             map,
-            centerPoint
+            city
         }
     },
     mounted() {
-        this.map = new BMapGL.Map('b-map')
-        this.map.centerAndZoom(this.centerPoint, 15)
-        this.map.enableScrollWheelZoom(true)
-        this.map.setMapStyleV2({
-            styleId: '073b242e4be0bc629bf89b95f6870c3c'
-        })
+        this.initMap()
     },
     methods: {
-
+        initMap() {
+            this.map = new BMapGL.Map('b-map')
+            this.map.centerAndZoom(this.city.center, 11)
+            this.map.enableScrollWheelZoom(true)
+            this.map.setMapStyleV2({
+                styleId: '073b242e4be0bc629bf89b95f6870c3c'
+            })
+            this.initPolygon(this.city.name)
+        },
+        initPolygon(city) {
+            let that = this
+            let boundary = new BMapGL.Boundary()
+            boundary.get(city, function (res) {
+                that.map.clearOverlays()
+                res.boundaries.forEach(item => {
+                    let points = item.split(';')
+                    let bPoints = points.map(item2 => {
+                        let loc = item2.split(',')
+                        return new BMapGL.Point(parseFloat(loc[0]), parseFloat(loc[1]))
+                    })
+                    let polygon = new BMapGL.Polygon(bPoints, {
+                        fillColor: '#024d6e',
+                        strokeColor: '#adfbfd',
+                        strokeWeight: 2,
+                        strokeOpacity: 0.5
+                    })
+                    that.map.addOverlay(polygon)
+                })
+            });
+        }
     }
 }
 </script>
