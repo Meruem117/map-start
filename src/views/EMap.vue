1bec09a33ea4b2aba0ffe5f5a50d46708e1c8f8e
Meruem117 <1572677319@qq.com>
2023-08-17 21:19:38 +0800

    add echarts

---
+++ src/views/EMap.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="page">
+        <div class="map-box">
+            <div id="e-map" class="map"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'EMap',
+    setup() {
+    },
+    mounted() {
+        this.initMap()
+    },
+    methods: {
+        initMap() {
+        },
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.page {
+    width: 100%;
+    height: 100%;
+    padding: 0px;
+    margin: 0px;
+
+    .map-box {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+
+        .map {
+            width: 100%;
+            height: 100%;
+        }
+    }
+}
+</style>
